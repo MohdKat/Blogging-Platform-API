1a216a923e832b02a17b07c04b36ebc37cca2c8b
MohdKat <mwk21@mail.aub.edu>
2026-05-24 14:37:33 +0300

    Created the Blog struct containing all of the metadata and added json tags for marshalling and unmarshalling http requests and responses

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	// "fmt"
-	// "net/http"
+	"fmt"
+	"net/http"
+	"time"
 )
+
+
+//Created Blog struct and Json tags for marshalling and unmarshalling data
+type Blog struct {
+
+	ID string             `json:"id"`
+	Title string          `json:"title"`
+	Content string		  `json:"content"`
+	Tags []string         `json:"tags"`
+	CreatedAt time.Time   `json:"createdAt"`
+	UpdatedAt time.Time   `json:"updatedAt"`
+}
 
 func main() {
 	
