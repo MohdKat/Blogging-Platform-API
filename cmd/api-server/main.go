d6f191913383f0e35e46239b7ce5f096e158f037
MohdKat <mwk21@mail.aub.edu>
2026-05-24 18:41:06 +0300

    Used docker to install postgres image ran the image and created a RDS , then established a connection between the main package and the database

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"log"
+	"os"
 	"time"
-)
+	"database/sql"
 
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
 
 //Created Blog struct and Json tags for marshalling and unmarshalling data
 type Blog struct {
@@ -19,5 +23,30 @@ type Blog struct {
 }
 
 func main() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user_name := os.Getenv("USER_NAME")
+	db_name := os.Getenv("DB_NAME")
+	db_password := os.Getenv("POSTGRES_PASSWORD")
+
+	connStr := fmt.Sprintf("postgres://%s:%s@localhost:5432/%s?sslmode=disable", user_name, db_password, db_name)
+
+	db, err := sql.Open("postgres", connStr)
 	
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/MohdKat/Blogging-Platform-API.git
 
 go 1.26.3
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+)
